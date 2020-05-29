c321917cfbdc9c8e446498b0f9e77a67b9841512
cesar <antonioc@titaniumsolutions.com.mx>
2020-05-29 12:56:09 -0500

    agregando ejemplos de cast en varios tipos de datos

---
+++ HolaMundo_con_eclipseIDE_7.java
@@ -0,0 +1,78 @@
+package mx.com.titaniumsolutions.HolaMundo;
+//upper camel case nombre de clase
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo cruel :)");
+		
+		//lower camel case en el nombre de variables 
+		//enteros
+		byte edad = 127;
+		short year = 32767;
+		int id_User = 1001;
+		long id_twitter = 2456276356273L;
+		
+		//punto flotante
+		
+		float diametro = 34.25F;
+		double precio = 1234.4273276437634762;
+		
+		// tipo char (texto)
+		
+		char genero = 'f';
+		
+		//tipo de dato lógico
+		
+		
+		boolean isvisible = true;
+		boolean funciona = false;
+		
+		//ejemplo de como se pueden escribir las variables 
+		
+		int variable = 1;
+		int Variable = 2;
+		int $variable = 3;
+		int _variable = 4;
+		
+		System.out.println(variable);
+		System.out.println(Variable);
+		System.out.println($variable);
+		System.out.println(_variable);
+		
+		
+		//constantes
+		
+		int VALOR = 0;
+		int VALOR_MAXIMO = 0;
+		
+		
+		//casteo automatico 
+		byte b = 6;
+		short s = b;
+		
+		//casteo manual 
+		b = (byte) s; 
+		
+		//otro ejemplo de casteo automatico
+		int i =1;
+		double d = 2.5; 
+		
+		//otro ejemplo de casteo manual 
+		i = (int) d; 
+	
+		System.out.println(i);
+		
+		//ejemplo de cast para castear tipo entero a tipo char
+		int codigo = 97;
+		char ascii = (char) codigo; 
+		
+		System.out.println(ascii);
+				
+		
+		
+		
+
+	}
+
+}
