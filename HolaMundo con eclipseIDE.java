787dacaf502784f9932fdfa4fa0402e22c60c4d1
cesar <antonioc@titaniumsolutions.com.mx>
2020-05-29 10:25:19 -0500

    agregando mi HolaMundo hecho en eclipseIDE

---
+++ HolaMundo con eclipseIDE.java
@@ -0,0 +1,11 @@
+package mx.com.titaniumsolutions.HolaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo cruel :)");
+
+	}
+
+}
