28dc908edaccb890848c6c46fd9637a64eb2349a
cesar <antonioc@titaniumsolutions.com.mx>
2020-05-29 11:40:31 -0500

    agregando mi HolaMundo hecho en eclipseIDE donde defino variables con tipo de dato logico

---
+++ HolaMundo_con_eclipseIDE_4.java
@@ -0,0 +1,34 @@
+package mx.com.titaniumsolutions.HolaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo cruel :)");
+		
+		//enteros
+		byte edad = 127;
+		short year = 32767;
+		int id_User = 1001;
+		long id_twitter = 2456276356273L;
+		
+		//punto flotante
+		
+		float diametro = 34.25F;
+		double precio = 1234.4273276437634762;
+		
+		// tipo char (texto)
+		
+		char genero = 'f';
+		
+		//tipo de dato lógico
+		
+		
+		boolean isvisible = true;
+		boolean funciona = false;
+		
+		
+
+	}
+
+}
