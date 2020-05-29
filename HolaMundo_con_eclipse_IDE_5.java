9ee5c981b899742ec5faeb0937dc1fe375d72f2b
cesar <antonioc@titaniumsolutions.com.mx>
2020-05-29 12:00:41 -0500

    definiendo como se pueden escribir variables y constantes

---
+++ HolaMundo_con_eclipse_IDE_5.java
@@ -0,0 +1,51 @@
+package mx.com.titaniumsolutions.HolaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo cruel :)");
+		
+		//enteros
+		byte edad = 127;
+		short year = 32767;
+		int id_User = 1001;
+		long id_twitter = 2456276356273L;
+		
+		//punto flotante
+		
+		float diametro = 34.25F;
+		double precio = 1234.4273276437634762;
+		
+		// tipo char (texto)
+		
+		char genero = 'f';
+		
+		//tipo de dato lógico
+		
+		
+		boolean isvisible = true;
+		boolean funciona = false;
+		
+		//ejemplo de naming 
+		
+		int variable = 1;
+		int Variable = 2;
+		int $variable = 3;
+		int _variable = 4;
+		
+		System.out.println(variable);
+		System.out.println(Variable);
+		System.out.println($variable);
+		System.out.println(_variable);
+		
+		
+		//constantes
+		
+		int VALOR = 0;
+		int VALOR_MAXIMO = 0;
+		
+
+	}
+
+}
