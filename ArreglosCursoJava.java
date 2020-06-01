ff9c2694fa8e7fad5b7c33097255ec5610cbc198
cesar <antonioc@titaniumsolutions.com.mx>
2020-06-01 12:30:08 -0500

    agregando un ejemplo de como pueden anidarse ciclos for

--- ArreglosCursoJava.java
+++ ArreglosCursoJava.java
@@ -113,7 +113,16 @@ public class ArreglosCursoJava {
 			System.out.println(j);
 					
 		}
-		
+		//ciclo for anidado
+		//Definimos un array de 3 filas x 5 columnas
+        int array[][]={{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}};
+ 
+        //Recorremos el array multidimensional
+        for (int o=0;o<array.length;o++){
+            for(int j=0;j<array[0].length;j++){
+                System.out.println(array[o][j]);
+            }
+        }
 		
 		
 		
