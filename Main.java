d827cf9d76bf7f41f6d4fc10a201c3a519012af1
cesar <antonioc@titaniumsolutions.com.mx>
2020-06-02 11:59:33 -0500

    importando movie y creando una pelicula para instanciar la classe como ejemplo

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 package mx.com.titaniumsolutions.Amazon_Vewer;
 
+import mx.com.titaniumsolutions.Amazon_Vewer.model.*;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Movie movie = new Movie();
+		movie.tittle = "coco";
+		movie.timeviewed = 120;
 
 		int exit = 0;
 		
@@ -17,6 +22,29 @@ public class Main {
 			System.out.println("4. Magazines");
 			System.out.println("0. Exit");
 			
+			byte opcion = 5;
+			switch (opcion) {
+			case 1: 
+				System.out.println("lista de movies:");
+				break;
+			case 2: 
+				System.out.println("lista de series:");
+				break;
+			case 3: 
+				System.out.println("lista de books:");
+				break;
+			case 4: 
+				System.out.println("lista de magazines:");
+				break;
+			case 0: 
+				System.out.println("gracias por usar Amazon VIewer ");
+				break;
+			default:
+				System.out.println("opcion invalida");
+				break; 
+			
+				
+			}
 			
 		} while (exit != 0);
 		
